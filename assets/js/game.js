26da682ad38c81d5eebbff2d149453592e8dcc77
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-02-18 01:13:22 -0800

    first step MVP

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,9 +12,10 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
+    while(enemyHealth > 0 && playerHealth > 0){
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     if (promptFight.toLowerCase() === "fight") {
-        window.alert("welcome to Robot Gladiators!");
+        //window.alert("welcome to Robot Gladiators!");
         enemyHealth = enemyHealth - playerAttack;
         console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
         playerHealth = playerHealth - enemyAttack;
@@ -30,27 +31,32 @@ var fight = function(enemyName) {
     }
         if (playerHealth <= 0) {
         window.alert(playerName + " has died!");
+        break;
     }
         else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     
     }
-}
-else if (promptFight.toLowerCase === "skip") {
+} else if (promptFight.toLowerCase() === "skip") {
     window.alert(playerName + " has chosen to skip the fight!");
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
     if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         playerMoney = playerMoney - 2;
+        console.log("playerMoney ", playerMoney);
+        break;
     }else {
         fight();
     }
 } else {
     window.alert("You need to choose a valid option. Try again!");
 }
-    
+    }    
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-}
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+        fight(pickedEnemyName); 
+    }
+
